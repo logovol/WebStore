fda202633a07b95e1b206093c0f71fd29634d187
logovol <pasha1985@inbox.ru>
2022-05-15 22:05:09 +0300

    Первый контроллер

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            return Content("Index action");
+        }
+
+        public IActionResult Greetings(string? id)
+        {
+            return Content($"Hello from first controller - {id}");
+        }
+    }
+}
