e4673a57702584eec5163b9c7adf85b358e7201d
logovol <pasha1985@inbox.ru>
2022-05-15 23:02:31 +0300

    Сотрудники

--- WebStore/Controllers/HomeController.cs
+++ WebStore/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        private static readonly List<Employee> __Employees = new()
+        {
+            new Employee { Id = 1, LastName = "Иванов", Name = "Иван", Patronymic = "Иванович", Age = 23 },
+            new Employee { Id = 2, LastName = "Петров", Name = "Пётр", Patronymic = "Петрович", Age = 27 },
+            new Employee { Id = 3, LastName = "Сидоров", Name = "Сидор", Patronymic = "Сидорович", Age = 18 },
+        };
+
         public IActionResult Index()
         {
             return View();
@@ -13,6 +21,11 @@ namespace WebStore.Controllers
         public IActionResult Greetings(string? id)
         {
             return Content($"Hello from first controller - {id}");
+        }
+
+        public IActionResult Employees()
+        {
+            return View(__Employees);
         }
     }
 }
