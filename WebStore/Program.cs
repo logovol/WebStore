8e47726c5f16abe14d2f3081a65e04b0f353492c
logovol <pasha1985@inbox.ru>
2022-05-15 21:23:49 +0300

    Приветствие из файла конфигурации

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -6,6 +6,7 @@ var app = builder.Build();
 // из файла конфигурации appsettings.json
 //var greetings = app.Configuration["ServerGreetings"];
 //app.MapGet("/", () => "Hello World!");
+//таким образом можно изменить информацию динамически
 app.MapGet("/", () => app.Configuration["ServerGreetings"]);
 
 app.Run();
