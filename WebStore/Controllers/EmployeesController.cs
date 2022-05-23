d6609c0170362e3b66a15a230d0ff34a3d3549f5
logovol <pasha1985@inbox.ru>
2022-05-23 20:20:02 +0300

    Заготовки действий контроллера сотрудников

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,34 +1,36 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
 using WebStore.Models;
+using WebStore.Services.Interfaces;
 
-namespace WebStore.Controllers
+namespace WebStore.Controllers;
+
+//[Route("Staff/{action=Index}/{Id?}")]
+public class EmployeesController : Controller
 {
-    //[Route("Staff/{action=Index}/{Id?}")]
-    public class EmployeesController : Controller
+    private readonly IEmployeesData _Employees;
+    
+    public EmployeesController(IEmployeesData Employees) => _Employees = Employees;
+
+    public IActionResult Index()
     {
-        private static readonly List<Employee> __Employees = new()
-        {
-            new Employee { Id = 1, LastName = "Иванов", Name = "Иван", Patronymic = "Иванович", Age = 23 },
-            new Employee { Id = 2, LastName = "Петров", Name = "Пётр", Patronymic = "Петрович", Age = 27 },
-            new Employee { Id = 3, LastName = "Сидоров", Name = "Сидор", Patronymic = "Сидорович", Age = 18 },
-        };
-
-        public IActionResult Index()
-        {
-            return View(__Employees);
-        }
-
-        //[Route("Staff/Info/{Id}")]
-        //[Route("Info/{Id}")]        
-        //[Route("[controller]/Info/{Id}")]
-        public IActionResult Details(int Id)
-        {
-            var employee = __Employees.FirstOrDefault(x => x.Id == Id);
-            if (employee is null)
-                return NotFound();
-
-            return View(employee);
-        }
+        var employees = _Employees.GetAll();
+        return View(employees);
     }
+
+    //[Route("Staff/Info/{Id}")]
+    //[Route("Info/{Id}")]        
+    //[Route("[controller]/Info/{Id}")]
+    public IActionResult Details(int Id)
+    {
+        var employee = _Employees.GetById(Id);
+        if (employee is null)
+            return NotFound();
+
+        return View(employee);
+    }
+
+    public IActionResult Create() => View();
+    public IActionResult Edit(int Id) => View();
+    public IActionResult Delete(int Id) => View();
 }
