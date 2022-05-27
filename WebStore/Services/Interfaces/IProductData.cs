795be625397ef801a3e2191f273d18725abdf92d
logovol <pasha1985@inbox.ru>
2022-05-27 16:35:30 +0300

    Заготовка для сервиса товаров

---
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebStore.Domain.Entities;
+
+namespace WebStore.Services.Interfaces
+{
+    public interface IProductData
+    {
+        IEnumerable<Section> GetSections();
+        IEnumerable<Brand> GetBrands();        
+    }
+}
