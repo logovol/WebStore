caef657ba373b6bdf3c5f061c8ed0b25e4d25b3b
logovol <pasha1985@inbox.ru>
2022-05-27 15:52:07 +0300

    Заготовка визуальных компонентов

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components;
+
+public class BrandsViewComponent : ViewComponent
+{
+    // можно выбирать представление, если их несколько
+    // public IViewComponentResult Invoke() => View("Name");
+    public IViewComponentResult Invoke() => View();
+}
---
+++ WebStore/Components/SectionsViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components;
+
+//базовый класс ViewComponent дает доп инструменты, которые могут пригодиться
+public class SectionsViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke() => View();
+    //public async Task<IViewComponentResult> InvokeAsync() => View();
+}
