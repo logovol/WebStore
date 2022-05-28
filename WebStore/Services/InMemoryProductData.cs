7e7f80ee83eb519b93a37883d7d3d03ef3a8d8df
logovol <pasha1985@inbox.ru>
2022-05-28 22:55:58 +0300

    Доработка логики сервиса каталога товаров - выдача списка товаров по фильтру

--- WebStore/Services/InMemoryProductData.cs
+++ WebStore/Services/InMemoryProductData.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 using WebStore.Services.Interfaces;
 
@@ -14,4 +15,20 @@ public class InMemoryProductData : IProductData
 {
     public IEnumerable<Section> GetSections() => TestData.Sections;
     public IEnumerable<Brand> GetBrands() => TestData.Brands;
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+    {
+        IEnumerable<Product> query = TestData.Products;
+
+        //if(Filter != null && Filter.SectionId != null)
+        //    query = query.Where(x => x.SectionId == Filter.SectionId);
+
+        if (Filter is { SectionId: { } section_id })
+            query = query.Where(x => x.SectionId == section_id);
+
+        if (Filter is { BrandId: { } brand_id })
+            query = query.Where(x => x.BrandId == brand_id);
+
+        return query;
+    }
 }
--- WebStore/Services/Interfaces/IProductData.cs
+++ WebStore/Services/Interfaces/IProductData.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using WebStore.Data;
+using WebStore.Domain;
 using WebStore.Domain.Entities;
 
 namespace WebStore.Services.Interfaces
@@ -11,6 +13,7 @@ namespace WebStore.Services.Interfaces
     public interface IProductData
     {
         IEnumerable<Section> GetSections();
-        IEnumerable<Brand> GetBrands();        
+        IEnumerable<Brand> GetBrands();
+        IEnumerable<Product> GetProducts(ProductFilter? Filter = null);   
     }
 }
