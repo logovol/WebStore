93a33ed27bfa2ac71fc028e7d9f5bc3a9b6cb68d
logovol <pasha1985@inbox.ru>
2022-05-31 10:15:44 +0300

    Визуализация каталога товаров

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -1,9 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using WebStore.Domain;
+using WebStore.Services.Interfaces;
+using WebStore.ViewModels;
+
 namespace WebStore.Controllers
 {
     public class CatalogController : Controller
     {
-        public IActionResult Index() => View(); // shop.html
+        private readonly IProductData _ProductData;
+        public CatalogController(IProductData productData) => _ProductData = productData;
+
+        public IActionResult Index(int? SectionId, int? BrandId)
+        {
+            var filter = new ProductFilter
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+            };
+
+            var products = _ProductData.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                BrandId = BrandId,
+                SectionId = SectionId,
+                Products = products
+                    .OrderBy(p => p.Order)
+                    .Select(p => new ProductViewModel
+                    {
+                       Id = p.Id,
+                       Name = p.Name,
+                       Price = p.Price,
+                       ImageUrl = p.ImageUrl,
+                    }),
+            });
+        }
     }
 }
