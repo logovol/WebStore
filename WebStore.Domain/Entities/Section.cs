e9fefb905c9f43e79415fa2965eba7320f02c914
logovol <50459467+logovol@users.noreply.github.com>
2022-06-27 21:45:57 +0300

    Модель данных БД

--- WebStore.Domain/Entities/Section.cs
+++ WebStore.Domain/Entities/Section.cs
@@ -1,10 +1,23 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+using Microsoft.EntityFrameworkCore;
+
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities;
 
+// уникальные имены не нужны
+[Index(nameof(Name), IsUnique = false)]
 public class Section : NamedEntity, IOrderedEntity
 {
     public int Order { get; set; }
     public int? ParentId { get; set; }
+
+    // можно не писать EF сама создаст
+    [ForeignKey(nameof(ParentId))]
+    // навигационное свойство
+    public Section? Parent { get; set; }
+
+    public ICollection<Product> Products { get; set; } = new HashSet<Product>();
 }
