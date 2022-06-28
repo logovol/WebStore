763e087958885c058eb05be5b96bd85e7d59d684
logovol <50459467+logovol@users.noreply.github.com>
2022-06-28 16:55:59 +0300

    Сервис каталога товаров для БД

---
+++ WebStore/Services/InSQL/SqlProductData.cs
@@ -0,0 +1,31 @@
+﻿using WebStore.DAL.Context;
+using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Services.Interfaces;
+
+namespace WebStore.Services.InSQL;
+
+public class SqlProductData : IProductData
+{
+    private readonly WebStoreDB _db;
+
+    public SqlProductData(WebStoreDB db) => _db = db;
+
+    public IEnumerable<Brand> GetBrands() => _db.Brands/*.AsEnumerable()*/;
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+    {
+        IQueryable<Product> query = _db.Products;
+
+        if (Filter is { SectionId: { } section_id })
+            query = query.Where(x => x.SectionId == section_id);
+
+        if (Filter is { BrandId: { } brand_id })
+            query = query.Where(x => x.BrandId == brand_id);
+
+        return query;
+    }
+
+    // AsEnumerable - выполняется запрос и из него начинается чтение данных. ToArray сразу выгружаются все данные в память (мб outofmemory)
+    public IEnumerable<Section> GetSections() => _db.Sections/*.AsEnumerable()*/;
+}
