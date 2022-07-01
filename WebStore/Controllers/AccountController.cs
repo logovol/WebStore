3cc8802ad638526230e1c083fbd6ca1de2c8c07c
logovol <50459467+logovol@users.noreply.github.com>
2022-07-01 13:04:46 +0300

    Заготовка контроллера для системы Identity

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Domain.Entities.Identity;
+
+namespace WebStore.Controllers;
+
+public class AccountController : Controller
+{
+    private readonly UserManager<User> _UserManager;
+    private readonly SignInManager<User> _SignInManager;
+    private readonly ILogger<AccountController> _Logger;
+
+    public AccountController(
+        UserManager<User> UserManager,
+        SignInManager<User> SignInManager,
+        ILogger<AccountController> Logger)
+    {
+        _UserManager = UserManager;
+        _SignInManager = SignInManager;
+        _Logger = Logger;
+    }
+
+    public IActionResult Register() => View();
+
+    public IActionResult Login() => View();
+
+    public IActionResult Logout() => View();
+
+    public IActionResult AccessDenied() => View();
+}
