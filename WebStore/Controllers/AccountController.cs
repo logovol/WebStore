9cc708031d06d9ce1a8e51387ed3f70b454f782c
logovol <50459467+logovol@users.noreply.github.com>
2022-07-02 21:53:56 +0300

    Выход из системы

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -99,7 +99,16 @@ public class AccountController : Controller
         return View(Model);
     }
 
-    public IActionResult Logout() => View();
+    public async Task<IActionResult> Logout()
+    {
+        var user_name = User.Identity!.Name;
+
+        await _SignInManager.SignOutAsync();
+
+        _Logger.LogInformation("Пользователь {0} вышел из системы", user_name);
+        
+        return RedirectToAction("Index", "Home");
+    }
 
     public IActionResult AccessDenied() => View();
 }
