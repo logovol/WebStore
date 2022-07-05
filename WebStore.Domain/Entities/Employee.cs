6a04a9654522a409396baaa34e8af6c147074cb0
logovol <50459467+logovol@users.noreply.github.com>
2022-07-05 14:28:39 +0300

    Сотрудник - сущность!

--- WebStore.Domain/Entities/Employee.cs
+++ WebStore.Domain/Entities/Employee.cs
@@ -1,11 +1,16 @@
-﻿namespace WebStore.Domain.Entities;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Employee
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities;
+
+public class Employee : Entity
 {
-    public int Id { get; set; }
+    [Required]
     public string Name { get; set; } = null!;
+    [Required]
     public string LastName { get; set; } = null!;
-    public string Patronymic { get; set; } = null!;
+    public string? Patronymic { get; set; } = null!;
     public int Age { get; set; }
 
     public override string ToString() => $"(id:{Id}){LastName}{Name}{Patronymic} - age:{Age}";
