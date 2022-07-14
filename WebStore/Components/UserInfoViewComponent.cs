510e1e0a29ac1b23f13deeae6f517380bbd7ccdb
logovol <50459467+logovol@users.noreply.github.com>
2022-07-14 11:11:32 +0300

    Визуальный компонент для пользователя

---
+++ WebStore/Components/UserInfoViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Components;
+
+public class UserInfoViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke() => User.Identity!.IsAuthenticated
+        ? View("UserInfo")
+        : View();
+}
