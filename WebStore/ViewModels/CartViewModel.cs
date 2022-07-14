c3847dea1c3a8177b14f113e9a410377cea0b5fa
logovol <50459467+logovol@users.noreply.github.com>
2022-07-14 14:42:09 +0300

    Определение сервиса Корзина

---
+++ WebStore/ViewModels/CartViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace WebStore.ViewModels;
+
+public class CartViewModel
+{
+    public IEnumerable<(ProductViewModel Product, int Quantity)> items { get; set; } = null!;
+
+    public int ItemsCount => items.Sum(item => item.Quantity);
+    
+    public decimal TotalPrice => items.Sum(item => item.Quantity * item.Product.Price);
+}
