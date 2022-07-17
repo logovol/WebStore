f8e72f422e23ca9432212d6f74ec433b790a7371
logovol <50459467+logovol@users.noreply.github.com>
2022-07-17 21:05:31 +0300

    Ссылки на действия контроллера Корзины

--- WebStore/Controllers/CartController.cs
+++ WebStore/Controllers/CartController.cs
@@ -11,5 +11,23 @@ namespace WebStore.Controllers
         public CartController(ICartService cartService) => _CartService = cartService;
         
         public IActionResult Index() => View(_CartService.GetViewModel()); // cart.html
+
+        public IActionResult Add(int Id)
+        {
+            _CartService.Add(Id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Decrement(int Id)
+        {
+            _CartService.Decrement(Id);
+            return RedirectToAction("Index", "Cart");
+        }
+
+        public IActionResult Remove(int Id)
+        {
+            _CartService.Remove(Id);
+            return RedirectToAction("Index", "Cart");
+        }
     }
 }
