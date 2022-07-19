1e1f06c6824e5ae7f6f36ff62158b250beae19b5
logovol <50459467+logovol@users.noreply.github.com>
2022-07-19 17:45:53 +0300

    Отображение списка товаров в контроллере области администратора

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,8 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using WebStore.Services.Interfaces;
+
 namespace WebStore.Areas.Admin.Controllers;
 
 public class ProductsController : Controller
 {
-    public IActionResult Index() => View();
+    private readonly IProductData _ProductData;
+    private readonly ILogger<ProductsController> _Logger;
+
+    public ProductsController(IProductData ProductData, ILogger<ProductsController> Logger)
+    {
+        _ProductData = ProductData;
+        _Logger = Logger;
+    }
+    public IActionResult Index()
+    {
+        var products = _ProductData.GetProducts();
+        return View(products);
+    }
 }
