a26929410b9ac5d7ebbdaa1bd1a05aafb7a22923
logovol <50459467+logovol@users.noreply.github.com>
2022-07-21 16:47:03 +0300

    Заглушки для методов редактирования и удаления товров в области администратора

--- WebStore/Areas/Admin/Controllers/ProductsController.cs
+++ WebStore/Areas/Admin/Controllers/ProductsController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
+using WebStore.Domain.Entities.Identity;
 using WebStore.Services.Interfaces;
 
 namespace WebStore.Areas.Admin.Controllers;
 
+[Authorize(Roles = Role.Administrators)]
 public class ProductsController : Controller
 {
     private readonly IProductData _ProductData;
@@ -18,5 +21,25 @@ public class ProductsController : Controller
     {
         var products = _ProductData.GetProducts();
         return View(products);
+    }
+
+    public IActionResult Edit(int id)
+    {
+        // заглушка
+        return View();
+        // logovol ht
+        // 1. Добавить в IProductData метод Edit и Delete
+        // 2. В сервисах InMemoryProductData и SqlProductData реализовать эти методы
+        // 3. Во вьюхе Edit вызвать этот метод и отредактировать по route-Id
+    }
+
+    public IActionResult Delete(int id)
+    {
+        // заглушка
+        return View();
+        // logovol ht
+        // 1. Добавить в IProductData метод Edit и Delete
+        // 2. В сервисах InMemoryProductData и SqlProductData реализовать эти методы
+        // 3. 3. Во вьюхе Delete вызвать этот метод и удалить по route-Id
     }
 }
