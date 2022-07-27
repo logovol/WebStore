713fb7cf669b8aa5add19618943210737bc70460
logovol <50459467+logovol@users.noreply.github.com>
2022-07-27 11:31:20 +0300

    Логика контроллера корзины для формирования заказа

---
+++ WebStore/ViewModels/CartOrderViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebStore.ViewModels;
+
+public class CartOrderViewModel
+{
+    public CartViewModel Cart { get; set; } = null!;
+    public OrderViewModel Order { get; set; } = new();
+}
