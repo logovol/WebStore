066862ccadcd98616a4ce280576cdc57ad570d1f
logovol <50459467+logovol@users.noreply.github.com>
2022-08-08 11:52:53 +0300

    Заготовка контроллера тестового WebAPI

---
+++ Services/WebStore.WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+//[Route("api/[controller]")]
+[Route("api/values")]
+public class ValuesController : ControllerBase
+{
+    private static readonly Dictionary<int, string> _Values = Enumerable.Range(1,10)
+        .Select(i => (Id: i, Value: $"Value-{i}"))
+        .ToDictionary(v => v.Id, v => v.Value);
+}
