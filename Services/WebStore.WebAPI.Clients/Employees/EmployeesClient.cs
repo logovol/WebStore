7374bfd471141e28ae9b5fc78d647d09b045d93b
logovol <50459467+logovol@users.noreply.github.com>
2022-08-11 18:42:50 +0300

    Заготовка базового клиента WebAPI управления сотрудниками

---
+++ Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -0,0 +1,10 @@
+﻿using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Employees;
+
+public class EmployeesClient : BaseClient
+{
+    public EmployeesClient(HttpClient Client, string Address /* специально оставлена ошибка */) : base(Client, "api/employees")
+    {
+    }
+}
