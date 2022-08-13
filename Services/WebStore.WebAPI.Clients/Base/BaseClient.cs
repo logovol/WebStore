318facf78cb4f5d612fd6d908ed914d930a55afe
logovol <50459467+logovol@users.noreply.github.com>
2022-08-13 12:57:54 +0300

    Доработка базового клиента и добавление возможности отмены асинхронных операций

--- Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
+++ Services/WebStore.WebAPI.Clients/Base/BaseClient.cs
@@ -1,4 +1,8 @@
-﻿namespace WebStore.WebAPI.Clients.Base;
+﻿
+using System.Net;
+using System.Net.Http.Json;
+
+namespace WebStore.WebAPI.Clients.Base;
 
 public abstract class BaseClient
 {
@@ -8,5 +12,58 @@ public abstract class BaseClient
     {
         Http = Client;
         this.Address = Address;
+    }
+
+    protected T? Get<T>(string url) => GetAsync<T>(url).Result;
+    
+    protected async Task<T?> GetAsync<T>(string url, CancellationToken Cancel = default)
+    {
+        var response = await Http.GetAsync(url, Cancel).ConfigureAwait(false);
+
+        //if (response.StatusCode == HttpStatusCode.NoContent)
+        //    return default;
+
+        //if (response.StatusCode == HttpStatusCode.NotFound)
+        //    return default;
+
+        //var result = await response.EnsureSuccessStatusCode().Content.ReadFromJsonAsync<T>();
+
+        //return result;
+
+        switch (response.StatusCode)
+        {
+            case HttpStatusCode.NoContent:
+            case HttpStatusCode.NotFound:
+                return default;
+            default:
+                var result = await response
+                    .EnsureSuccessStatusCode()
+                    .Content.ReadFromJsonAsync<T>(cancellationToken: Cancel);
+                return result;
+        }
+    }
+
+    protected HttpResponseMessage Post<T>(string url, T value) => PostAsync(url, value).Result;
+
+    protected async Task<HttpResponseMessage> PostAsync<T>(string url, T value, CancellationToken Cancel = default)
+    {
+        var response = await Http.PostAsJsonAsync(url, value, Cancel).ConfigureAwait(false);
+        return response.EnsureSuccessStatusCode();
+    }
+
+    protected HttpResponseMessage Put<T>(string url, T value) => PutAsync(url, value).Result;
+
+    protected async Task<HttpResponseMessage> PutAsync<T>(string url, T value, CancellationToken Cancel = default)
+    {
+        var response = await Http.PutAsJsonAsync(url, value, Cancel).ConfigureAwait(false);
+        return response.EnsureSuccessStatusCode();
+    }
+
+    protected HttpResponseMessage Delete(string url) => DeleteAsync(url).Result;
+
+    protected async Task<HttpResponseMessage> DeleteAsync(string url, CancellationToken Cancel = default)
+    {
+        var response = await Http.DeleteAsync(url, Cancel).ConfigureAwait(false);
+        return response;
     }
 }
--- Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
+++ Services/WebStore.WebAPI.Clients/Employees/EmployeesClient.cs
@@ -1,10 +1,47 @@
-﻿using WebStore.WebAPI.Clients.Base;
+﻿using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
 
 namespace WebStore.WebAPI.Clients.Employees;
 
-public class EmployeesClient : BaseClient
+public class EmployeesClient : BaseClient, IEmployeesData
 {
     public EmployeesClient(HttpClient Client, string Address /* специально оставлена ошибка */) : base(Client, "api/employees")
     {
+    }
+
+    public int Add(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Delete(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Edit(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Employee> Get(int Skip, int Take)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Employee> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Employee? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int GetCount()
+    {
+        throw new NotImplementedException();
     }
 }
