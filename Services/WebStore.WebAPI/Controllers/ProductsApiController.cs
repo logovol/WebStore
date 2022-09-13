ae5e5962b87654cab52072716292593f3b275536
logovol <50459467+logovol@users.noreply.github.com>
2022-09-13 16:31:17 +0300

    Наивная реализация WebAPI сервиса каталога товаров

--- Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
+++ Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using WebStore.Domain;
 using WebStore.Interfaces.Services;
 
 namespace WebStore.WebAPI.Controllers;
@@ -16,4 +17,36 @@ public class ProductsApiController : ControllerBase
         _ProductData = ProductData;
         _Logger = Logger;
     }
+
+    [HttpGet("sections")]
+    public IActionResult GetSections() => Ok(_ProductData.GetSections());
+
+
+    [HttpGet("sections/{Id:int}")]
+    public IActionResult GetSectionById(int Id) => _ProductData.GetSectionById(Id) is { } section
+        ? Ok(section)
+        : NotFound(new { Id });
+
+    [HttpGet("brands")]
+    public IActionResult GetBrands() => Ok(_ProductData.GetBrands());
+
+    [HttpGet("brands/{Id:int}")]
+    public IActionResult GetBrandById(int Id) => _ProductData.GetBrandById(Id) is { } brand
+        ? Ok(brand)
+        : NotFound(new { Id });
+
+    [HttpPost]
+    public IActionResult GetProducts([FromBody] ProductFilter filter)
+    {
+        var products = _ProductData.GetProducts(filter);
+
+        if (products.Any())
+            return Ok(products);
+        return NoContent();
+    }
+
+    [HttpGet("{Id:int}")]
+    public IActionResult GetProductById(int Id) =>_ProductData.GetProductById(Id) is { } product
+        ? Ok(product)
+        : NotFound(new { Id });
 }
