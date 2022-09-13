112f788ff73fda83842e4e43cf87bf044335e81f
logovol <50459467+logovol@users.noreply.github.com>
2022-09-13 12:23:56 +0300

    Заготовка контроллера каталога товаров

---
+++ Services/WebStore.WebAPI/Controllers/ProductsApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/products")]
+public class ProductsApiController : ControllerBase
+{
+    private readonly IProductData _ProductData;
+    private readonly ILogger<ProductsApiController> _Logger;
+
+    public ProductsApiController(IProductData ProductData, ILogger<ProductsApiController> Logger)
+    {
+        _ProductData = ProductData;
+        _Logger = Logger;
+    }
+}
