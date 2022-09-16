cbd6732464689dd115debcd960893b99c73f971f
logovol <50459467+logovol@users.noreply.github.com>
2022-09-16 17:13:56 +0300

    Заготовка контроллера для сериса заказов

---
+++ Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using WebStore.Interfaces.Services;
+
+namespace WebStore.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/orders")]
+public class OrdersApiController
+{
+    private readonly IOrderService _OrderService;
+    private readonly ILogger<OrdersApiController> _Logger;
+
+    public OrdersApiController(IOrderService OrderService, ILogger<OrdersApiController> Logger)
+    {
+        _OrderService = OrderService;
+        _Logger = Logger;
+    }
+}
