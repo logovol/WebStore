010255c1297f0779b9e54e6cfc2657f8b5f7fff0
logovol <50459467+logovol@users.noreply.github.com>
2022-09-16 11:28:34 +0300

    Заготовка клиента для WebAPI каталога товаров

---
+++ Services/WebStore.WebAPI.Clients/ProductsClient.cs
@@ -0,0 +1,43 @@
+﻿using WebStore.Domain;
+using WebStore.Domain.Entities;
+using WebStore.Interfaces.Services;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients;
+
+public class ProductsClient : BaseClient, IProductData
+{
+    public ProductsClient(HttpClient Client) : base(Client, "api/products")
+    {
+    }
+
+    public Brand? GetBrandById(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Brand> GetBrands()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Product? GetProductById(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetProducts(ProductFilter? Filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Section? GetSectionById(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Section> GetSections()
+    {
+        throw new NotImplementedException();
+    }
+}
