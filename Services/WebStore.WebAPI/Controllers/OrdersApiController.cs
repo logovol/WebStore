aa7c1a8371392135299436018d580813dcf92f6d
logovol <pasha1985@inbox.ru>
2023-05-03 14:24:41 +0300

    Контроллер WebAPI сервиса заказов

--- Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
+++ Services/WebStore.WebAPI/Controllers/OrdersApiController.cs
@@ -1,12 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-
+using WebStore.Domain.DTO;
 using WebStore.Interfaces.Services;
 
 namespace WebStore.WebAPI.Controllers;
 
 [ApiController]
 [Route("api/orders")]
-public class OrdersApiController
+public class OrdersApiController : ControllerBase
 {
     private readonly IOrderService _OrderService;
     private readonly ILogger<OrdersApiController> _Logger;
@@ -15,5 +15,33 @@ public class OrdersApiController
     {
         _OrderService = OrderService;
         _Logger = Logger;
+    }
+
+    [HttpGet("user/{UserName}")]
+    public async Task<IActionResult> GetUserOrders(string UserName)
+    {
+        var orders = await _OrderService.GetUserOrdersAsync(UserName);
+        if (orders.Any())
+            return Ok(orders.ToDTO());
+        return NoContent();
+    }
+
+    [HttpGet("{Id:int}")]
+    public async Task<IActionResult> GetOrderById(int Id)
+    {
+        var order = await _OrderService.GetOrderByIdAsync(Id);
+        if (order is null)
+            return NotFound();
+        return Ok(order.ToDTO());
+    }
+
+    [HttpPost("{UserName}")]
+    public async Task<IActionResult> CreateOrder(string UserName, [FromBody] CreateOrderDTO Model)
+    {
+        var cart = Model.Items.ToCartViewModel();
+        var order_model = Model.Order;
+
+        var order = _OrderService.CreateOrderAsync(UserName, cart, order_model);
+        return CreatedAtAction(nameof(GetOrderById), new { order.Id } ,order);
     }
 }
