ad15908a8cd48beb37d3beeb0a7f8390b77c296a
logovol <pasha1985@inbox.ru>
2023-05-18 13:02:44 +0300

    Заготовка клиентов WebApi системы Identity

---
+++ Services/WebStore.WebAPI.Clients/Identity/RolesClient.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity;
+
+public class RolesClient : BaseClient
+{
+	public RolesClient(HttpClient Client) : base(Client, WebApiAddresses.V1.Identity.Roles)
+	{
+
+	}
+}
---
+++ Services/WebStore.WebAPI.Clients/Identity/UsersClient.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Interfaces;
+using WebStore.WebAPI.Clients.Base;
+
+namespace WebStore.WebAPI.Clients.Identity;
+
+public class UsersClient : BaseClient
+{
+    public UsersClient(HttpClient Client) : base(Client, WebApiAddresses.V1.Identity.Users)
+    {
+
+    }
+}
