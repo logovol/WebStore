55f84a23843738caa881c642a4068fcd2cdca8e5
logovol <pasha1985@inbox.ru>
2023-05-19 15:27:06 +0300

    DTO-модели объектов операций Identity

---
+++ Common/WebStore.Domain/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Claims;
+
+namespace WebStore.Domain.DTO.Identity;
+
+public class ClaimDTO : UserDTO
+{
+    public IEnumerable<ClaimDTO> Claims { get; init; } = null!;
+}
+
+public class ReplaceClaimDTO : UserDTO
+{
+    // исходный
+    public Claim Claim { get; init; } = null!;
+    // на который производится замена
+    public Claim NewClaim { get; init; } = null!;
+}
