8adb323b88112a8f2d84dd922954b3a5023776c7
logovol <pasha1985@inbox.ru>
2023-06-01 13:15:20 +0300

    Документация к коду

--- Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
+++ Services/WebStore.WebAPI/Controllers/EmployeesApiController.cs
@@ -6,6 +6,7 @@ using WebStore.Interfaces.Services;
 
 namespace WebStore.WebAPI.Controllers;
 
+/// <summary>Управление сотрудниками</summary>
 [ApiController]
 [Route(WebApiAddresses.V1.Employees)]
 public class EmployeesApiController : ControllerBase
@@ -18,14 +19,17 @@ public class EmployeesApiController : ControllerBase
         _EmployeesData = EmployeesData;
         _Logger = Logger;
     }
-
+    /// <summary>Количество сотрудников</summary>
+    /// <returns></returns>
     [HttpGet("count")] // GET -> api/employees/count
     public IActionResult GetCount()
     {
         var result = _EmployeesData.GetCount();
         return Ok(result);
     }
-
+    
+    /// <summary>Полный список сотрудников</summary>
+    /// <returns></returns>
     [HttpGet] // GET -> api/employees/count
     public IActionResult GetAll()
     {
@@ -38,6 +42,11 @@ public class EmployeesApiController : ControllerBase
         return Ok(result);
     }
 
+
+    /// <summary>Фрагмент списка сотрудников</summary>
+    /// <param name="Skip">Пропускаемое количество элементов в начале списка</param>
+    /// <param name="Take">Количество элементов в выборке</param>
+    /// <returns></returns>
     [HttpGet("[[{Skip:int}/{Take:int}]]")] // GET -> api/employees/[2:4] пропустить 2, получить 4 элемента
     [HttpGet("{Skip:int}/{Take:int}")]   // GET -> api/employees/2/4  пропустить 2, получить 4 элемента
     public IActionResult Get(int Skip, int Take)
@@ -52,6 +61,9 @@ public class EmployeesApiController : ControllerBase
         return Ok(result);
     }
 
+    /// <summary>Сотрудник с заданным идентификатором</summary>
+    /// <param name="id">Идентификатор сотрудника</param>
+    /// <returns></returns>
     [HttpGet("{id:int}")]
     public IActionResult GetById(int id)
     {
@@ -61,6 +73,9 @@ public class EmployeesApiController : ControllerBase
             :Ok(result);
     }
     
+    /// <summary>Добавление нового сотрудника</summary>
+    /// <param name="employee">Добавляемый сотрудник</param>
+    /// <returns></returns>
     [HttpPost]
     public IActionResult Add([FromBody] Employee employee)
     {
@@ -68,6 +83,9 @@ public class EmployeesApiController : ControllerBase
         return CreatedAtAction(nameof(GetById), new { Id = id }, employee);
     }
 
+    /// <summary>Внесение изменений в информацию о сотруднике</summary>
+    /// <param name="employee">Структура с новой информацией о сотруднике</param>
+    /// <returns></returns>
     [HttpPut]
     public IActionResult Edit([FromBody] Employee employee)
     {
@@ -77,6 +95,9 @@ public class EmployeesApiController : ControllerBase
         return NotFound(false);
     }
 
+    /// <summary>Удаление сотрудника</summary>
+    /// <param name="Id">Идентификатор сотрудника</param>
+    /// <returns></returns>
     [HttpDelete("{Id:int}")]
     public IActionResult Delete(int Id)
     {
