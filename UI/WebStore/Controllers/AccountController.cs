ffb5d938c1708741afb57b22730c6a5233871f52
logovol <pasha1985@inbox.ru>
2023-06-09 12:04:32 +0300

    Подавление лишних записей в журнале с помощью фильтра в файле конфигурации

--- UI/WebStore/Controllers/AccountController.cs
+++ UI/WebStore/Controllers/AccountController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Diagnostics;
+
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -43,15 +45,24 @@ public class AccountController : Controller
             UserName = Model.UserName,
         };
 
+        _Logger.LogTrace("Начало процесса регистрации нового пользователя {0)", Model.UserName);
+        var timer = Stopwatch.StartNew();
+
+        using var logger_scope = _Logger.BeginScope("Регистрация нового пользователя {0}", Model.UserName);
+
         var creation_result = await _UserManager.CreateAsync(user, Model.Password);
-        if(creation_result.Succeeded)
+
+        if (creation_result.Succeeded)
         {
-            _Logger.LogInformation("Пользователь {0} зарегистрирован", user);
+            _Logger.LogInformation("Пользователь {0} зарегистрирован за {1} мс", user, timer.ElapsedMilliseconds);
 
             await _UserManager.AddToRoleAsync(user, Role.Users);
 
+            _Logger.LogInformation("Пользователю {0} назначена роль {1}. {2} мс", user, Role.Users, timer.ElapsedMilliseconds);
+
             // Вход в систему. false - на один сеанс
             await _SignInManager.SignInAsync(user, false);
+            _Logger.LogTrace("Пользователю {0} вошёл в систему. {1} мс", user, timer.ElapsedMilliseconds);
 
             return RedirectToAction("Index", "Home");
         }
@@ -60,7 +71,7 @@ public class AccountController : Controller
             ModelState.AddModelError(string.Empty, error.Description);
 
         var error_info = string.Join(", ", creation_result.Errors.Select(e => e.Description));
-        _Logger.LogWarning("Ошибка при регистрации пользователя {0}:{1}", user, error_info);
+        _Logger.LogWarning("Ошибка при регистрации пользователя {0}:({1} мс):{2}", user, timer.ElapsedMilliseconds, error_info);
 
         return View(Model);
     }
@@ -79,24 +90,30 @@ public class AccountController : Controller
         if (!ModelState.IsValid)
             return View(Model);
 
+        _Logger.LogTrace("Начат вход в систему пользователя {0}", Model.UserName);
+
+        using var logger_scope = _Logger.BeginScope("Вход в систему пользователя {0}", Model.UserName);
+
+        var timer = Stopwatch.StartNew();
+
         var login_result = await _SignInManager.PasswordSignInAsync(
             Model.UserName,
             Model.Password,
             Model.RememberMe,
             lockoutOnFailure: true);
 
-        if(login_result.Succeeded)
+        if (login_result.Succeeded)
         {
-            _Logger.LogInformation("Пользователь {0} успешно вошёл в систему", Model.UserName);
+            _Logger.LogInformation("Пользователь {0} успешно вошёл в систему. {1} мс", Model.UserName, timer.ElapsedMilliseconds);
 
             // делать редирект просто так нельзя. мб подмена на другой сайт и cookies уйдет злоумышленнику
             //return Redirect(Model.ReturnUrl);
-            
+
             // Так нужно делать
             //if(Url.IsLocalUrl(Model.ReturnUrl))
             //    return Redirect(Model.ReturnUrl);
             //return RedirectToAction("Index", "Home");
-        
+
             // Можно написать проще
             return LocalRedirect(Model.ReturnUrl ?? "/");
         }
@@ -104,7 +121,7 @@ public class AccountController : Controller
         // Увеомление поьзователя, если он не вошел в систему
         ModelState.AddModelError("", "Неверное имя пользователя или пароль");
 
-        _Logger.LogWarning("Ошибка входа пользователя {0} - неверное имя или пароль", Model.UserName);
+        _Logger.LogWarning("Ошибка входа пользователя {0} - неверное имя или пароль. {1} мс.", Model.UserName, timer.ElapsedMilliseconds);
 
         return View(Model);
     }
@@ -116,13 +133,13 @@ public class AccountController : Controller
         await _SignInManager.SignOutAsync();
 
         _Logger.LogInformation("Пользователь {0} вышел из системы", user_name);
-        
+
         return RedirectToAction("Index", "Home");
     }
 
     [AllowAnonymous]
     public IActionResult AccessDenied(string? ReturnUrl)
-    { 
+    {
         ViewBag.ReturnUrl = ReturnUrl;
         return View();
     }
