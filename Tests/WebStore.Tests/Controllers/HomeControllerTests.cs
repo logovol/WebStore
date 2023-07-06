b4337d6279bdb1ee1a513d40832d1f4c218a03e8
logovol <pasha1985@inbox.ru>
2023-07-06 17:18:09 +0300

    Первый модульный тест

---
+++ Tests/WebStore.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+using WebStore.Controllers;
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers;
+
+[TestClass]
+public class HomeControllerTests
+{
+    [TestMethod]
+    public void Contacts_returns_with_View()
+    {
+        // AAA = A-A-A Arrange - Act - Assert
+        // Подготовка данных - их сравнение и проверяем результат
+
+        #region Arrange
+        var controller = new HomeController(null!);
+        #endregion
+
+        #region Act
+        var result = controller.Contacts();
+        #endregion
+
+        #region Assert
+        var view_result = Assert.IsType<ViewResult>(result);
+
+        // Assert.Equal(nameof(HomeController.Contacts), view_result.ViewName); // неправильное утверждениие
+        Assert.Null(view_result.ViewName);
+        #endregion
+    }
+
+}
