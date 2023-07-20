605c70f3c002b7facee3a74b22148834f4d69a60
logovol <pasha1985@inbox.ru>
2023-07-20 13:15:32 +0300

    Заготовка таг-хелпера

---
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebStore.TagHelpers;
+
+[HtmlTargetElement(Attributes = AttributeName)]
+public class ActiveRoute : TagHelper
+{
+    private const string AttributeName = "is-active-route";
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        output.Attributes.RemoveAll(AttributeName);
+    }
+}
