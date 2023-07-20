f73ec246457ae555069d1074c77973c1d3c4a886
logovol <pasha1985@inbox.ru>
2023-07-20 13:58:22 +0300

    Свойства таг-хелпера для доступа к данным, размещенным в разметке

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace WebStore.TagHelpers;
 
@@ -6,6 +8,19 @@ namespace WebStore.TagHelpers;
 public class ActiveRoute : TagHelper
 {
     private const string AttributeName = "is-active-route";
+
+    [HtmlAttributeName("asp-controller")]
+    public string Controller { get; set; } = null!;
+
+    [HtmlAttributeName("asp-action")]
+    public string Action { get; set; } = null!;
+
+    [HtmlAttributeName("asp-all-route-data", DictionaryAttributePrefix = "asp-route-")]
+    public Dictionary<string, string> RouteValues { get; set; } = new(StringComparer.OrdinalIgnoreCase);
+
+    [ViewContext, HtmlAttributeNotBound]
+    public ViewContext ViewContext { get; set; } = null!;
+
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.Attributes.RemoveAll(AttributeName);
