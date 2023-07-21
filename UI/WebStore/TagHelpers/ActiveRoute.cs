b30a3dd50637d4b51e2e353edcaf2cd09aa99ca1
logovol <pasha1985@inbox.ru>
2023-07-21 14:46:27 +0300

    Основная логика таг-хелпера подсветки ссылки

--- UI/WebStore/TagHelpers/ActiveRoute.cs
+++ UI/WebStore/TagHelpers/ActiveRoute.cs
@@ -24,5 +24,45 @@ public class ActiveRoute : TagHelper
     public override void Process(TagHelperContext context, TagHelperOutput output)
     {
         output.Attributes.RemoveAll(AttributeName);
+
+        if (IsActive())
+            MakeActive(output);
+    }    
+
+    private bool IsActive()
+    {
+        var route_values = ViewContext.RouteData.Values;
+
+        var route_controller = route_values["controller"]?.ToString();
+        var route_action = route_values["action"]?.ToString();
+
+        if (Action is { Length: > 0 } action && !string.Equals(action, route_action))
+            return false;
+
+        if (Controller is { Length: > 0 } controller && !string.Equals(controller, route_controller))
+            return false;
+
+        foreach (var (key, value) in RouteValues)
+        {
+            if(!route_values.ContainsKey(key) || route_values[key]?.ToString() != value.ToString())
+                return false;
+        }
+
+        return true;
+    }
+    
+    private static void MakeActive(TagHelperOutput output)
+    {
+        var class_attribute = output.Attributes.FirstOrDefault(attr => attr.Name == "class");
+
+        if (class_attribute is null)
+            output.Attributes.Add("class", "active");
+        else
+        {
+            if (class_attribute.Value?.ToString()?.Contains("active") == true)
+                return;
+
+            output.Attributes.SetAttribute("class", $"{class_attribute.Value} active");
+        }
     }
 }
