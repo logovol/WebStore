4fb9010201fec7b62c1ba8ef614e1352b56abeb7
logovol <pasha1985@inbox.ru>
2023-08-18 12:20:34 +0300

    Логика добавления товара в корзину посредством скрипта

---
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -0,0 +1,42 @@
+﻿Cart = {
+    _properties: {
+        getCartViewLink: "",
+        addToCartLink: ""        
+    },
+
+    init: function(properties) {
+        $.extend(Cart._properties, properties);
+        $(".add-to-cart").click(Cart.addToCart);
+    },
+
+    addToCart: function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const id = button.data("id");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function (response) {
+                console.log(response.message);
+
+                Cart.showToolTip(button);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("addToCart fail"); });
+    },
+
+    showToolTip: function(button) {
+        button.tooltip({ title: "Добавлено в корзину" }).tooltip("show");
+        setTimeout(function () {
+            button.tooltip("destroy");
+        }, 500);
+    },
+
+    refreshCartView: function () {
+        $.get(Cart._properties.getCartViewLink)
+            .done(function (cartHtml) {
+                $("#cart-container").html(cartHtml);
+            })
+            .fail(function () { console.log("refreshCartView fail"); });
+    }
+}
