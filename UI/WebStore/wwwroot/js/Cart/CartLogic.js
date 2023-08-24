ec0757e4a83ebc839c944b6e0b29d69da660dc76
logovol <pasha1985@inbox.ru>
2023-08-24 18:50:49 +0300

    Логика скрипта для кнопки "+" в представлении корзины

--- UI/WebStore/wwwroot/js/Cart/CartLogic.js
+++ UI/WebStore/wwwroot/js/Cart/CartLogic.js
@@ -7,17 +7,17 @@
         removeFromCartLink: ""
     },
 
-    init: function(properties) {
+    init: function (properties) {
         $.extend(Cart._properties, properties);
         Cart.initEvents();
     },
 
-    initEvents: function () {
+    initEvents: function() {
         $(".add-to-cart").click(Cart.addToCart);
-        //$(".cart_quantity_up").click(Cart.incrementItem);
+        $(".cart_quantity_up").click(Cart.incrementItem);
         //$(".cart_quantity_down").click(Cart.decrementItem);
         //$(".cart_quantity_delete").click(Cart.removeItem);
-    };
+    },
 
     addToCart: function (e) {
         e.preventDefault();
@@ -35,7 +35,7 @@
             .fail(function () { console.log("addToCart fail"); });
     },
 
-    showToolTip: function(button) {
+    showToolTip: function (button) {
         button.tooltip({ title: "Добавлено в корзину" }).tooltip("show");
         setTimeout(function () {
             button.tooltip("destroy");
@@ -48,5 +48,53 @@
                 $("#cart-container").html(cartHtml);
             })
             .fail(function () { console.log("refreshCartView fail"); });
+    },
+
+    incrementItem: function (e) {
+        e.preventDefault();
+
+        const button = $(this);
+        const id = button.data("id");
+
+        const tr = button.closest("tr");
+
+        $.get(Cart._properties.addToCartLink + "/" + id)
+            .done(function (response) {
+                console.log(response.message);
+
+                const count = parseInt($(".cart_quantity_input", tr).val());
+                $(".cart_quantity_input", tr).val(count + 1);
+
+                Cart.refreshPrice(tr);
+                Cart.refreshCartView();
+            })
+            .fail(function () { console.log("incrementItem fail"); });
+    },
+
+    refreshPrice: function (tr) {
+        const count = parseInt($(".cart_quantity_input", tr).val());
+        const price = parseFloat($(".cart_price", tr).data("price"));
+
+        const totalPrice = price * count;
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+
+        const cartTotalPrice = $(".cart_total_price", tr);
+        cartTotalPrice.html(value);
+        cartTotalPrice.data("price", totalPrice);
+
+        Cart.refreshTotalPrice();
+    },
+
+    refreshTotalPrice: function () {
+        var totalPrice = 0;
+
+        $(".cart_total_price").each(function () {
+            const price = parseFloat($(this).data("price"));
+            totalPrice += price;
+        });
+
+        const value = totalPrice.toLocaleString("ru-RU", { style: "currency", currency: "RUB" });
+        $("#total-order-price").html(value);
     }
 }
